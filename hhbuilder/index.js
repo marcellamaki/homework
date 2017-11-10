7445b9a4193510ea1c1677f7d57c3a2960073f01
Marcella <marcella@Kevins-Air.home>
2017-11-10 17:11:52 -0500

    started notes and elements

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,1 +1,18 @@
 // your code goes here ...
+document.addEventListener('DOMContentLoaded',
+  function(event) {
+    //console.log("I'm connected!") - checking to make sure files are structured correctly and index.js file is accesssible in index.html
+
+    //adding event listeners to form items
+
+    var form = document.forms[0];
+    var ageField = form.querySelector('input[name="age"]').value;
+    var age = ageField;
+
+
+    document.addEventListener("submit", function(event){
+      event.preventDefault()
+      console.log(age)
+    })
+
+  })
