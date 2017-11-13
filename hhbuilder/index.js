59c207214456eb839427197d7aa72917fd2ec870
Marcella <marcella@Kevins-Air.home>
2017-11-13 16:09:23 -0500

    added event listeners

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,18 +1,31 @@
 // your code goes here ...
-document.addEventListener('DOMContentLoaded',
-  function(event) {
+window.onload = function(event) {
     //console.log("I'm connected!") - checking to make sure files are structured correctly and index.js file is accesssible in index.html
 
     //adding event listeners to form items
+    const myHousehold = []
 
-    var form = document.forms[0];
-    var ageField = form.querySelector('input[name="age"]').value;
-    var age = ageField;
+    let form = document.forms[0];
+    let ageField;
+    let relationshipOption;
+    let smoker;
+    let householdList;
 
-
-    document.addEventListener("submit", function(event){
-      event.preventDefault()
-      console.log(age)
+    document.addEventListener("change", function(event) {
+      ageField = form.querySelector('input[name="age"]').value;
+      // console.log(ageField) - checks that values are accurate
     })
 
-  })
+    document.addEventListener("change", function(event) {
+      relationshipOption = form.querySelector('select[name="rel"]').value;
+      // console.log(relationshipOption) - checks that values are accurate
+    })
+
+    document.addEventListener("change", function(event) {
+      smoker = form.querySelector('input[name="smoker"]').checked;
+      // console.log(smoker)
+    })
+
+    
+
+  }
